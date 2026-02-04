a64c733aab208f04541719a7552588ac073bf4aa
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2026-02-03 19:48:26 -0800

    Add gitignore, Create Class Shells and Create Dice Class

--- Deadwood.java
+++ Deadwood.java
@@ -1,1 +1,33 @@
+import java.util.*;
 
+public class Deadwood{
+
+    // Cannot be name system our causes System.out.prinln to error due to reference bug 
+    public class Game{
+
+    }
+    public class Set{
+
+    }
+    public class Player{
+
+    }
+
+    // Dice Class
+    public class Dice{
+        // Create a Random Instance
+        private static Random random = new Random();
+        public static int roll(){
+            return random.nextInt(6) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start Game");
+        Deadwood game = new Deadwood();
+        Dice dice = game.new Dice();
+        System.out.println(dice.roll());
+
+    }
+
+}
