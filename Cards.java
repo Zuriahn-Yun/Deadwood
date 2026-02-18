d8f9149c8a80b07c47e281fab36fac1460bb7720
Amy <phama8@wwu.edu>
2026-02-17 22:54:10 -0800

    Code for trying to parse cards.xml, also changed Scenes.java to Cards.java for simple

---
+++ Cards.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * TODO Parse cards.xml
+ * MANAGES CARDS, BUDGET, AND ROLES AVAILABLE
+ */
+public class Cards {
+    private String name;
+    private int budget;
+    private int sceneNumber;
+    private String sceneDescription;
+    private ArrayList<Part> parts = new ArrayList<>();
+
+    // constructor, getters, setters
+    public Cards(String name, int budget, int sceneNumber, String description) {
+        this.name = name;
+        this.budget = budget;
+        this.sceneNumber = sceneNumber;
+        this.sceneDescription = description;
+    }
+
+    public void addRole(Part part) {
+        this.parts.add(part);
+    }
+
+    public int getBudget() { return budget; }
+    public ArrayList<Part> getParts() { return parts; }
+    public String getName() { return name; }
+}
